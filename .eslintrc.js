39c8d51c5e0cd47ba21bfe6902101ff593e7f668
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-09-07 10:31:54 -0300

    chore: improve code structure and organization

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  extends: [
+  'extends': [
     'plugin:react/recommended',
     'google',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
     'next/core-web-vitals',
   ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
-  plugins: ['react', 'jsx-a11y', 'prettier'],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    'jsx-a11y', 
+    'prettier'
+  ],
   rules: {
     'jsx-a11y/alt-text': 2,
     'jsx-a11y/anchor-has-content': 2,
